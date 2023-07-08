3eb4c14c00791a039e99a64596c537422ef2f70c
Victor Widell <victor.widell@divid.se>
2023-07-08 12:38:49 +0200

    Init StaticList

--- static-list.zig
+++ static-list.zig
@@ -1,6 +1,10 @@
 fn StaticList(comptime capacity: usize, comptime T: type) type {
     return struct {
         items: [capacity]T,
+
+        fn init() StaticList(capacity, T) {
+            return StaticList(capacity, T){ .items = undefined };
+        }
     };
 }
 
@@ -8,5 +12,10 @@ const expect = @import("std").testing.expect;
 
 test "Create StaticList" {
     const list = StaticList(2, u8){ .items = [_]u8{ 1, 2 } };
+    try expect(list.items.len == 2);
+}
+
+test "Init StaticList" {
+    const list = StaticList(2, u8).init();
     try expect(list.items.len == 2);
 }
