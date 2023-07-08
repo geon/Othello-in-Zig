c9e2cd5894a7f3c01de61dd6e3fba800959ed19c
Victor Widell <victor.widell@divid.se>
2023-07-08 12:43:33 +0200

    initial length

--- static-list.zig
+++ static-list.zig
@@ -1,9 +1,10 @@
 fn StaticList(comptime capacity: usize, comptime T: type) type {
     return struct {
         items: [capacity]T,
+        length: usize,
 
         fn init() StaticList(capacity, T) {
-            return StaticList(capacity, T){ .items = undefined };
+            return StaticList(capacity, T){ .items = undefined, .length = 0 };
         }
     };
 }
@@ -11,11 +12,16 @@ fn StaticList(comptime capacity: usize, comptime T: type) type {
 const expect = @import("std").testing.expect;
 
 test "Create StaticList" {
-    const list = StaticList(2, u8){ .items = [_]u8{ 1, 2 } };
+    const list = StaticList(2, u8){ .items = [_]u8{ 1, 2 }, .length = 2 };
     try expect(list.items.len == 2);
 }
 
 test "Init StaticList" {
     const list = StaticList(2, u8).init();
     try expect(list.items.len == 2);
+}
+
+test "initial length" {
+    const list = StaticList(2, u8).init();
+    try expect(list.length == 0);
 }
