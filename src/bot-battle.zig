df5dae2be75a06c459cdba6ae7b6f0fb5dad0365
Victor Widell <victor.widell@divid.se>
2023-11-22 18:14:15 +0100

    Run battles between all bots.

--- src/bot-battle.zig
+++ src/bot-battle.zig
@@ -6,6 +6,7 @@ const std = @import("std");
 const forceNotNull = @import("force-not-null.zig").forceNotNull;
 const Client = @import("client.zig").Client;
 const StaticList = @import("static-list.zig").StaticList;
+const allPairs = @import("all-pairs.zig").allPairs;
 
 fn compareStrings(_: void, lhs: []const u8, rhs: []const u8) bool {
     return std.mem.order(u8, lhs, rhs).compare(std.math.CompareOperator.lt);
@@ -25,43 +26,66 @@ pub fn main() !void {
 
     std.sort.insertion([]const u8, bots.items, {}, compareStrings);
 
-    const botNameA = bots.items[0];
-    const botNameB = bots.items[1];
+    const allBotPairs = try allPairs([]const u8, bots.items, allocator);
+    for (allBotPairs.items) |botPair| {
+        var resultPathBuffer = [_]u8{undefined} ** 100;
+        const resultPath = try std.fmt.bufPrint(&resultPathBuffer, "bot-results/{s}_{s}.txt", .{ botPair[0], botPair[1] });
 
-    var pathBufferA = [_]u8{undefined} ** 100;
-    var pathBufferB = [_]u8{undefined} ** 100;
-    const pathA = try std.fmt.bufPrint(&pathBufferA, "bots/{?s}", .{botNameA});
-    const pathB = try std.fmt.bufPrint(&pathBufferB, "bots/{?s}", .{botNameB});
+        var resultFile = std.fs.cwd().createFile(resultPath, .{ .exclusive = true }) catch |e|
+            switch (e) {
+            error.PathAlreadyExists => {
+                std.debug.print("Skipping {s} vs {s}\n", .{ botPair[0], botPair[1] });
+                continue;
+            },
+            else => return e,
+        };
+        defer resultFile.close();
 
-    var clientA = try Client.init(pathA, allocator);
-    var clientB = try Client.init(pathB, allocator);
-    defer clientA.deinit();
-    defer clientB.deinit();
+        std.debug.print("Current Battle: {s} vs {s}\n", .{ botPair[0], botPair[1] });
 
-    const numMatches = 100;
-    var scores = StaticList(numMatches, i32).init();
+        var pathBufferA = [_]u8{undefined} ** 100;
+        var pathBufferB = [_]u8{undefined} ** 100;
+        const pathA = try std.fmt.bufPrint(&pathBufferA, "bots/{?s}", .{botPair[0]});
+        const pathB = try std.fmt.bufPrint(&pathBufferB, "bots/{?s}", .{botPair[1]});
 
-    for (0..numMatches) |_| {
-        var board = Board.init();
-        while (true) {
-            if (board.gameOver) {
-                try scores.push(board.pieceBalance(1));
-                break;
+        var clientA = try Client.init(pathA, allocator);
+        var clientB = try Client.init(pathB, allocator);
+        defer clientA.deinit();
+        defer clientB.deinit();
+
+        const numMatches = 100;
+        var scores = StaticList(numMatches, i32).init();
+
+        for (0..numMatches) |_| {
+            var board = Board.init();
+            while (true) {
+                if (board.gameOver) {
+                    try scores.push(board.pieceBalance(1));
+                    std.debug.print(".", .{});
+                    break;
+                }
+
+                _ = board.doMove(try (if (board.player == 1) clientA else clientB).requestMove(board));
             }
-
-            _ = board.doMove(try (if (board.player == 1) clientA else clientB).requestMove(board));
         }
-    }
+        std.debug.print("\n", .{});
 
-    var sumScore: f32 = 0;
-    for (scores.getSlice()) |score| {
-        sumScore += @floatFromInt(score);
+        var sumScore: f32 = 0;
+        for (scores.getSlice()) |score| {
+            sumScore += @floatFromInt(score);
+        }
+        const averageScore = sumScore / numMatches;
+        var sumSquaredDeviation: f32 = 0;
+        for (scores.items[0..scores.length]) |score| {
+            sumSquaredDeviation += std.math.pow(f32, @as(f32, @floatFromInt(score)) - averageScore, 2);
+        }
+        const standardDeviation = std.math.sqrt(sumSquaredDeviation / numMatches);
+
+        var resultBuffer = [_]u8{undefined} ** 100;
+        const result = try std.fmt.bufPrint(&resultBuffer, "{d} {d}\n", .{ averageScore, standardDeviation });
+
+        std.debug.print("| Average Score: {d:.2} | Stdandard Deviation: {d:.2} |\n", .{ averageScore, standardDeviation });
+
+        _ = try resultFile.write(result);
     }
-    const averageScore = sumScore / numMatches;
-    var sumSquaredDeviation: f32 = 0;
-    for (scores.items[0..scores.length]) |score| {
-        sumSquaredDeviation += std.math.pow(f32, @as(f32, @floatFromInt(score)) - averageScore, 2);
-    }
-    const standardDeviation = std.math.sqrt(sumSquaredDeviation / numMatches);
-    std.debug.print("{d} {d}\n", .{ averageScore, standardDeviation });
 }
